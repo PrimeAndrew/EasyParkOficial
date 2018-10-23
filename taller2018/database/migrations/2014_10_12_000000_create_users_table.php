2c25568493d9ab59d09824bc0ebc642933e3ec2e
Wendy <wenguz1518@gmail.com>
2018-10-22 23:41:12 -0400

    Migrasion de users y pasword resets

--- taller2018/database/migrations/2014_10_12_000000_create_users_table.php
+++ taller2018/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,22 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+           /* $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->timestamps();*/
+            $table->increments('id_users');
+            $table->string('users_name')->nullable();
+            $table->string('users_lastname',45)->nullable();
+            $table->string('users_phone',10)->nullable();
+            $table->string('users_email')->nullable();
+            $table->string('users_type_doc');
+            $table->string('users_doc_number',10)->unique();
+            $table->string('users_password');
+            $table->string('users_status',45)->default('Activo');
             $table->timestamps();
         });
     }
