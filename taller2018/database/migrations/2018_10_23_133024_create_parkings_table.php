e58106225dc79e5d00cef2eca7075b5d55acd012
PrimeAndrew <raparicio71@gmail.com>
2018-10-23 09:40:48 -0400

    migration: create parkings table

---
+++ taller2018/database/migrations/2018_10_23_133024_create_parkings_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParkingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parkings', function (Blueprint $table) {
+            $table->increments('id_parkings');
+            //$table->unsignedInteger('id_zone_fk');
+            //$table->unsignedInteger('id_price_list_fk');
+            $table->string('parking_name',45);
+            $table->string('parking_address',150);
+            $table->unsignedInteger('total_spaces');
+            $table->time('open_hour');
+            $table->time('close_hour');
+            $table->decimal('latitude',10,7);
+            $table->decimal('longitud',10,7);
+            $table->timestamp('registry_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parkings');
+    }
+}
