6480c835201e9ab1347f9303c777150e7fc2b47c
PrimeAndrew <raparicio71@gmail.com>
2018-10-23 09:41:27 -0400

    migration: create reservations table

---
+++ taller2018/database/migrations/2018_10_23_133456_create_reservations_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id_reservations');
+            //$table->unsignedInteger('id_parking_space_fk');
+            //$table->unsignedInteger('id_users_roles_fk');
+            //$table->unsignedInteger('id_car_fk');
+            //$table->unsignedInteger('id_schedule_fk');
+            $table->timestamp('reservation_entry_date');
+            $table->timestamp('reservation_departure_date');
+            $table->decimal('amount',10,7);
+            $table->string('confirmation_code',45);
+            $table->string('reservation_state',45);
+            $table->timestamp('real_entry_date');
+            $table->timestamp('real_departure_date');
+            $table->string('reservation_type',45);
+            $table->unsignedInteger('client_score');
+            $table->unsignedInteger('parking_score');
+            $table->timestamp('date_mode_state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
