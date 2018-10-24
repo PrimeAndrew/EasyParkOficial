45e694d86216b36024d4721b24d58e3a38767016
jomcordova <jomcordova@gmail.com>
2018-10-23 21:04:40 -0400

    migrate: create_agenda_table

---
+++ taller2018/database/migrations/2018_10_23_191856_create_agendas_table.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josecordova
+ * Date: 23-10-18
+ * Time: 08:53 PM
+ */
+
+
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAgendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agendas', function (Blueprint $table) {
+            $table->increments('id_agendas');
+            //$table->unsignedInteger('id_schedules_fk');
+            $table->string('state_agenda',45);
+            $table->time('date_mod_agenda');
+            $table->unsignedInteger('user_id');
+            $table->string('host_mode_id',45);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agendas');
+    }
+}
