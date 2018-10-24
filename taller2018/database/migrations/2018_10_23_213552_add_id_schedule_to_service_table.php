8f9d620ab39726f354785e8c4c97a3144c5f8900
jomcordova <jomcordova@gmail.com>
2018-10-23 21:37:26 -0400

    migrate: add_id_schedule_to_service_table

---
+++ taller2018/database/migrations/2018_10_23_213552_add_id_schedule_to_service_table.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josecordova
+ * Date: 23-10-18
+ * Time: 09:35 PM
+ */
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIdSchedulesToServiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('services', function (Blueprint $table) {
+            $table->unsignedInteger('id_schedules_fk')->after('id_servicess');
+            $table->foreign('id_schedules_fk')->references('id_schedules')->on('schedules');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('services', function (Blueprint $table) {
+            $table->dropForeign(['id_schedules_fk']);
+            $table->dropColumn(['id_schedules_fk']);
+        });
+    }
+}
