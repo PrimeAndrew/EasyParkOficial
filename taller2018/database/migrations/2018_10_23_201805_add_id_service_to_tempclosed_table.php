bfb88b350c315c5313d66b9e3ad407e4a99e53d7
jomcordova <jomcordova@gmail.com>
2018-10-23 21:24:22 -0400

    migrate: add_id_service_to_temp_closed

---
+++ taller2018/database/migrations/2018_10_23_201805_add_id_service_to_tempclosed_table.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josecordova
+ * Date: 23-10-18
+ * Time: 09:18 PM
+ */
+
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIdServicesToTempClosedTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('temporal_closeds', function (Blueprint $table) {
+            $table->unsignedInteger('id_services_fk')->after('id_tempcloseds');
+            $table->foreign('id_services_fk')->references('id_services')->on('services');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('temporal_closeds', function (Blueprint $table) {
+            $table->dropForeign(['id_services_fk']);
+            $table->dropColumn(['id_services_fk']);
+        });
+    }
+}
