cb9b37fbc8c4bbb07e7fbbddd53d5be633ec1fde
jomcordova <jomcordova@gmail.com>
2018-10-23 21:12:51 -0400

    migrate: create_service_table

---
+++ taller2018/database/migrations/2018_10_23_192534_create_service_table.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josecordova
+ * Date: 23-10-18
+ * Time: 09:06 PM
+ */
+
+
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->increments('id_services');
+            //$table->unsignedInteger('id_parkings_fk');
+            //$table->unsignedInteger('id_schedules_fk');
+            $table->boolean('open');
+            $table->decimal('rate_per_hour',10,7);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
