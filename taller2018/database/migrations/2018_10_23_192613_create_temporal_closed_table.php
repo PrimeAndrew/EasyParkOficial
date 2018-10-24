4010d7dfc2d5d79bb93b561fd964cd96e8011096
jomcordova <jomcordova@gmail.com>
2018-10-23 21:17:09 -0400

    migrate: create_temporal_closed_table

---
+++ taller2018/database/migrations/2018_10_23_192613_create_temporal_closed_table.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josecordova
+ * Date: 23-10-18
+ * Time: 09:14 PM
+ */
+
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTemporalClosedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('temporal_closeds', function (Blueprint $table) {
+            $table->increments('id_tempcloseds');
+            //$table->unsignedInteger('id_services_fk');
+            $table->time('start_closing');
+            $table->time('end_closing');
+            $table->string('description',150);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('temporal_closeds');
+    }
+}
