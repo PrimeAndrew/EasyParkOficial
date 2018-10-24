0a8ba7ccdbed52d74f1678e966de8c6bcce3b102
joselujan <jos.d.t.b@gmail.com>
2018-10-24 14:49:54 -0400

    creacion de brand,creacion de car_model, creacion de la tabla cars_type, creacion de la tabla car

---
+++ taller2018/database/migrations/2018_10_24_182920_create_cars_model_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsModelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars_model', function (Blueprint $table) {
+            $table->increments('id_car_model');
+            $table->string('model',50);
+            $table->year('year',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars_model');
+    }
+}
