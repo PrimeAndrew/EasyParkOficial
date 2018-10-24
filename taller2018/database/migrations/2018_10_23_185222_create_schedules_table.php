9e0baae2be6ca8459f84789d2d55d47b0c04b3ca
jomcordova <jomcordova@gmail.com>
2018-10-23 20:24:46 -0400

    migrate: create_schedule_table

---
+++ taller2018/database/migrations/2018_10_23_185222_create_schedules_table.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josecordova
+ * Date: 23-10-18
+ * Time: 06:53 PM
+ */
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id_schedules');
+            $table->time('open_time');
+            $table->time('close_time');
+            $table->string('day',45);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
