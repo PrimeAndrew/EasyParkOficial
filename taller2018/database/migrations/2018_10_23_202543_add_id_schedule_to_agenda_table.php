b234680d8edf667efbc3868825eb5d4ff5546bd7
jomcordova <jomcordova@gmail.com>
2018-10-23 21:29:41 -0400

    migrate: add_id_schedule_to_agenda_table

---
+++ taller2018/database/migrations/2018_10_23_202543_add_id_schedule_to_agenda_table.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josecordova
+ * Date: 23-10-18
+ * Time: 09:25 PM
+ */
+
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIdSchedulesToAgendaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('agendas', function (Blueprint $table) {
+            $table->unsignedInteger('id_schedules_fk')->after('id_agendas');
+            $table->foreign('id_schedules_fk')->references('id_schedules')->on('schedules');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('agendas', function (Blueprint $table) {
+            $table->dropForeign(['id_schedules_fk']);
+            $table->dropColumn(['id_schedules_fk']);
+        });
+    }
+}
