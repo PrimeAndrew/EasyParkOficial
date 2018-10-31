26354f4d75c11bf8b811efe31250d9844d5fe4db
joselujan <jos.d.t.b@gmail.com>
2018-10-31 11:24:05 -0400

    migration :correccion add id_price_list_to_parkins

--- taller2018/database/migrations/2018_10_31_150507_create_add_id_price_list_to_parkings_table.php
+++ taller2018/database/migrations/2018_10_31_150507_create_add_id_price_list_to_parkings_table.php
@@ -14,8 +14,8 @@ class CreateAddIdPriceListToParkingsTable extends Migration
     public function up()
     {
         Schema::table('parkings', function (Blueprint $table) {
-            $table->unsignedInteger('id_parkings_fk')->after('id_parkings');
-            $table->foreign('id_parkings_fk')->references('id_parkings')->on('parkings');
+            $table->unsignedInteger('id_price_list_fk')->after('id_parkings');
+            $table->foreign('id_price_list_fk')->references('id_price_list')->on('price_list');
         });
     }
 
@@ -28,8 +28,8 @@ class CreateAddIdPriceListToParkingsTable extends Migration
     {
         //Schema::dropIfExists('id_price_list_to_parkings');
         Schema::table('parkings', function (Blueprint $table) {
-            $table->dropForeign(['id_parkings_fk']);
-            $table->dropColumn(['id_parkings_fk']);
+            $table->dropForeign(['id_price_list_fk']);
+            $table->dropColumn(['id_price_list_fk']);
         });
     }
 }
