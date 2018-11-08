6cf9405f31fff3c2c48af528378f7b77c3af7e66
PrimeAndrew <raparicio71@gmail.com>
2018-11-08 04:32:20 -0400

    roles seeder

---
+++ taller2018/database/seeds/Roles.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class Roles extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 5; $i++) {
+            DB::table('roles')->insert([
+                'roles_description' => str_random(5)
+            ]);
+
+        }
+    }
+}
