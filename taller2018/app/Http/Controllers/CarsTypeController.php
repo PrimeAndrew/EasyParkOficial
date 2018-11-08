5ec4f3e799df72d56f844ed6912262b9a8b41d3c
PrimeAndrew <raparicio71@gmail.com>
2018-11-08 01:20:03 -0400

    cars_type controller implementation

---
+++ taller2018/app/Http/Controllers/CarsTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Cars_type;
+use Illuminate\Http\Request;
+
+class CarsTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Cars_type  $cars_type
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cars_type $cars_type)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Cars_type  $cars_type
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cars_type $cars_type)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Cars_type  $cars_type
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cars_type $cars_type)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Cars_type  $cars_type
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cars_type $cars_type)
+    {
+        //
+    }
+}
