57f7d42ff158031ba60e88be47770bc16864fb81
Wendy <wenguz1518@gmail.com>
2018-11-13 02:20:16 -0400

    Buscar parqueo:se agrego tabla de informacion_incompleta

--- taller2018/app/Http/Controllers/GmapsController.php
+++ taller2018/app/Http/Controllers/GmapsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Gmaps;
+use App\Parking;
 use Illuminate\Http\Request;
 
 class GmapsController extends Controller
@@ -39,7 +40,7 @@ class GmapsController extends Controller
         // Una vez se conozca la posición del usuario
         $marker = array();
         $marker['position']='auto';
-        $marker['infowindow_content']='1-Hola';
+        $marker['infowindow_content']='Ubicasion Actual';
         $marker['icon']='http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=A|9999FF|000000';
         \Gmaps::add_marker($marker);
 
@@ -50,14 +51,14 @@ class GmapsController extends Controller
 //        $marker = array();
 //        $marker['position']='-16.5074250,-68.1461940';
 //        \Gmaps::add_marker($marker);
-
-
-
         $map = \Gmaps::create_map();
-
+        //$ubicasion=DB::table('parkings')->select('latitude','longitud')->get();
+        $parkings = Parking::orderBy('id_parkings','ASC')->paginate(2);
         //Devolver vista con datos del mapa
-        return view('reservations.searchParking', compact('map'));
+        return view('reservations.searchParking', compact('map','parkings'));
     }
+
+
 
     /**
      * Show the form for creating a new resource.
@@ -66,7 +67,7 @@ class GmapsController extends Controller
      */
     public function create()
     {
-        //
+
     }
 
     /**
@@ -88,7 +89,9 @@ class GmapsController extends Controller
      */
     public function show(Gmaps $gmaps)
     {
-        //
+        /*$zones = Zone::all();
+        $direcciones=Parking::all();
+        return view('reservations.searchParking',compact('zones','direcciones'));*/
     }
 
     /**
--- taller2018/resources/views/reservations/searchParking.blade.php
+++ taller2018/resources/views/reservations/searchParking.blade.php
@@ -11,13 +11,53 @@
                 <h2>Buscar Parqueo</h2>
                 <div class="row">
                     <div class="col-md-6">
+                        <!--div class="form-group">
+                            <select class="select2-A"  name="id_zones">
 
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <select class="select2-A"  name="dire">
+
+                            </select>
+                        </div>
+-->
+                        <div class="table-container">
+                            Parqueoes Encontrados:
+                            <table id="mytable" class="table table-bordred table-striped">
+                                <thead>
+                                <th>Nombre</th>
+                                <th>Direccion</th>
+                                <th>Total esapcio</th>
+                                <th>Hora apertura</th>
+                                <th>Hora Cierre</th>
+                                <th>Seleccionar</th>
+                                </thead>
+                                <tbody>
+
+                                    @foreach($parkings as $parking)
+                                        <tr>
+                                            <td>{{ $parking->parking_name }}</td>
+                                            <td>{{ $parking->parking_address}}</td>
+                                            <td>{{ $parking->total_spaces }}</td>
+                                            <td>{{ $parking->open_hour }}</td>
+                                            <td>{{ $parking->close_hour }}</td>
+                                            <td><a class="btn btn-primary btn-xs" href="/registerCliente" ><span class="glyphicon glyphicon-pencil"></span></a></td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+
+                            </table>
+
+                        </div>
                     </div>
                     <div class="col-md-6">
                         {!!$map['html']!!}
+
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 @endsection
