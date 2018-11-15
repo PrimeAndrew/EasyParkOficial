bf79724b9968dd06a23ec4f4fc533451fd264204
wenguz <wenguz1518@gmail.com>
2018-11-15 03:02:56 -0400

    Modelo_Controlador: se creo los archivos para Parking_Spaces

---
+++ taller2018/app/Http/Controllers/ParkingSpaceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ParkingSpace;
+use Illuminate\Http\Request;
+
+class ParkingSpaceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ParkingSpace  $parkingSpace
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ParkingSpace $parkingSpace)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ParkingSpace  $parkingSpace
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ParkingSpace $parkingSpace)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ParkingSpace  $parkingSpace
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ParkingSpace $parkingSpace)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ParkingSpace  $parkingSpace
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ParkingSpace $parkingSpace)
+    {
+        //
+    }
+}
