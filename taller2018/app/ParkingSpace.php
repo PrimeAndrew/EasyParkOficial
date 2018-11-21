d8d40698140a04760d5c83f15e9e2f988db523ad
wenguz <wenguz1518@gmail.com>
2018-11-21 14:57:38 -0400

    Modificasion en Gmapscontroller_parkingSpaces. Paralistado_arreglar.

--- taller2018/app/ParkingSpace.php
+++ taller2018/app/ParkingSpace.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class ParkingSpace extends Model
 {
-    //
+    protected $fillable = [
+        'id_parking_spaces',
+        'space_code',
+        'space_status',
+        'space_mod_date',
+        'id_parkings_fk'
+
+    ];
+
+    protected $primaryKey='id_parking_spaces';
+    protected $table = "parking_spaces";
 }
