3a315a677f30cc04bbc8bb4a92432d1543f79f95
PrimeAndrew <raparicio71@gmail.com>
2018-11-21 18:26:32 -0400

    Reservation of client model+ controller

---
+++ taller2018/app/Http/Controllers/ReservationClientController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Reservation_client;
+use Illuminate\Http\Request;
+
+class ReservationClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Reservation_client  $reservation_client
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Reservation_client $reservation_client)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Reservation_client  $reservation_client
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Reservation_client $reservation_client)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Reservation_client  $reservation_client
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Reservation_client $reservation_client)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Reservation_client  $reservation_client
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Reservation_client $reservation_client)
+    {
+        //
+    }
+}
