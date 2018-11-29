a3be3b27d2d0b6bfd610d0987ac94aed06e63b4b
PrimeAndrew <raparicio71@gmail.com>
2018-11-29 10:34:18 -0400

    rebuild

--- taller2018/app/Reservation_client.php
+++ taller2018/app/Reservation_client.php
@@ -7,13 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation_client extends Model
 {
     protected $fillable = [
-        'id_reservations',
         'entry_date',
         'entry_hour',
         'departure_date',
         'departure_hour',
-        //'amount',
-        //'confirmation_code',
+        'amount',
+        'confirmation_code',
         'id_car_fk',
         'id_parking_spaces_fk'
     ];
