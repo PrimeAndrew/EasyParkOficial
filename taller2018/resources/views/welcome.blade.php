d181d3a59845a852eb2d6dc17457264c581842be
wenguz <wenguz1518@gmail.com>
2018-12-01 19:59:08 -0400

    Cambio de nombre en diseño welcome

--- taller2018/resources/views/welcome.blade.php
+++ taller2018/resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Easy Park
                 </div>
 
                 <div class="links">
